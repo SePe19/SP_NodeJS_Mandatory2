90d1964b640620db12e097a2cc931bd47394a2d2
SePe19 <seba217i@stud.kea.dk>
2022-04-19 11:39:51 +0200

    Webshop incoming

--- routes/users.js
+++ routes/users.js
@@ -1,65 +1,97 @@
 import { Router } from "express";
 import User from "../models/user.js";
+import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import passport from "passport";
 import initializePassport from "../passport-config.js";
 
 const users = User.find();
 
-initializePassport(
-    passport,
-    email => users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
-);
-
 const router = Router();
 
-router.get("/", checkAuthenticated, (req, res) => {
-    res.render("auth/index");
+router.get("/register", (req, res) => {
+    res.render("auth/register");
 });
 
-router.get("/login", checkNotAuthenticated, (req, res) => {
+router.post("/register", (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: "Mail exists"
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user
+                            .save()
+                            .then(result => {
+                                res.redirect("/auth/login")
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
+                });
+            }
+        });
+});
+
+router.get("/login", (req, res) => {
     res.render("auth/login");
 });
 
-router.post("/login", checkNotAuthenticated, passport.authenticate("local", {
-    successRedirect: "/items",
-    failureRedirect: "/auth/login",
-    failureFlash: true
-}));
 
-router.get("/register", checkNotAuthenticated, (req, res) => {
-    res.render("auth/register", { user: new User() });
+router.post("/login", (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
+                return res.status(401).json({
+                    message: "Auth failed"
+                });
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: "Auth failed"
+                    });
+                }
+                if (result) {
+                    const token = jwt.sign(
+                        {
+                            email: user[0].email,
+                            userId: user[0]._id
+                        },
+                        process.env.JWT_KEY,
+                        {
+                            expiresIn: "1h"
+                        }
+                    );
+                    res.redirect("/items");
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
-
-router.post("/register", checkNotAuthenticated, async (req, res) => {
-    try {
-        let hashedPassword = await bcrypt.hash(req.body.password, 10);
-        let user = { email: req.body.email, password: hashedPassword };
-        user.save();
-        res.redirect("/auth/login");
-    } catch {
-        res.redirect("/auth/register");
-    }
-});
-
-router.delete("/logout", (req, res) => {
-    req.logOut();
-    res.redirect("/login");
-});
-
-function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect("auth/login");
-};
-
-function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return res.redirect("/items");
-    }
-    next();
-};
 
 export default router;
